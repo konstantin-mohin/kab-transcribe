54232a6689892be49ff173b9d1f02df140fd5977
cosmos <konstantin.mohin@wsc-sports.com>
2024-08-07 06:02:27 +0300

    v0.1

--- app.js
+++ app.js
@@ -3,16 +3,21 @@ const path = require("path");
 const fs = require("fs");
 const Busboy = require("busboy");
 const ffmpeg = require("fluent-ffmpeg");
+const OpenAI = require("openai");
 
 const app = express();
 
 const uploadsDir = path.join(__dirname, "uploads");
 
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
 // Create the 'uploads' directory if it doesn't exist
 fs.mkdirSync(uploadsDir, { recursive: true });
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public", "dist")));
 
 app.post("/upload", (req, res) => {
   const busboy = Busboy({ headers: req.headers });
@@ -34,12 +39,28 @@ app.post("/upload", (req, res) => {
       .audioCodec("libmp3lame")
       .format("mp3")
       .save(outputAudioPath)
-      .on("end", () => {
+      .on("end", async () => {
         console.log("File processing complete.");
         fs.unlinkSync(tempFilePath); // Clean up temp file
-        res.send("Upload and transcription complete");
+        // res.send("Upload and transcription complete");
+        try {
+          // Start OpenAI transcription (adjust settings as needed)
+          const transcriptionResult = await openai.audio.transcriptions.create({
+            file: fs.createReadStream(outputAudioPath),
+            model: "whisper-1",
+            response_format: "text",
+          });
+          console.log("File processing complete.", transcriptionResult);
+          res.send(transcriptionResult);
+
+          fs.unlinkSync(outputAudioPath);
+        } catch (error) {
+          console.error("Transcription error:", error);
+          res.status(500).send("Error during transcription");
+        }
       })
       .on("error", (err, stdout, stderr) => {
+        fs.unlinkSync(tempFilePath);
         console.error("FFmpeg Error:", err.message);
         console.error("FFmpeg STDOUT:", stdout);
         console.error("FFmpeg STDERR:", stderr);
